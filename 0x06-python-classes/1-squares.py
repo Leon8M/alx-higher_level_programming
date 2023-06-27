64823f969f37a04570097ff471e528af36402f75
Leon Munene <leonmunene254@gmail.com>
2023-06-27 09:13:18 -0700

    4

---
+++ 0x06-python-classes/1-squares.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that contains the Square class.
+"""
+
+
+class Square:
+    """
+    Class that defines a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
