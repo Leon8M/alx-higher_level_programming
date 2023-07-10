53df1b7797c509981a34b443d0add5dc538e3b33
Leon Munene <leonmunene254@gmail.com>
2023-07-10 03:41:09 -0700

    All

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module containing BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        Raises Exception
+        :return: Exception
+        """
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module containing BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        Raises Exception
+        :return: Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+        :param name: name
+        :param value: value
+        :return: nothing
+        :raises: TypeError if value is not int
+                 ValueError if value less or equal to zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
